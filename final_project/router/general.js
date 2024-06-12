8ec59d1bbb4dccc6b599bcf283f6e1765bf49fb2
Javilly <javi.valda@gmail.com>
2024-06-12 00:04:45 -0300

    Tasks10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios')
 
 const doesExist = (username)=>{
     let userswithsamename = users.filter((user)=>{
@@ -31,7 +32,8 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(300).json(books);
+    response = books
+  return res.status(300).json(response);
 });
 
 // Get book details based on ISBN
@@ -70,5 +72,67 @@ public_users.get('/title/:title',function (req, res) {
 public_users.get('/review/:isbn',function (req, res) {
   return res.status(300).json(books[req.params.isbn].reviews);
 });
+
+
+
+// Task 10
+async function getBooks() {
+    let books = []
+    try {
+      ;({ data: books } = await axios({
+        method: 'GET',
+        url: `http://localhost:5000`,
+      }))
+    } catch (error) {
+      console.log(error);
+    }
+
+    console.log(books);
+}
+
+// Task 11
+async function getBooksByISBN(isbn) {
+  let books = []
+  try {
+    ;({ data: books } = await axios({
+      method: 'GET',
+      url: `http://localhost:5000/isbn/${isbn}`,
+    }))
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log(books);
+}
+
+// Task 12
+async function getBooksByAuthor(author) {
+  let books = []
+  try {
+    ;({ data: books } = await axios({
+      method: 'GET',
+      url: `http://localhost:5000/author/${author}`,
+    }))
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log(books);
+}
+
+// Task 13
+async function getBooksByTitle(title) {
+  let books = []
+  try {
+    ;({ data: books } = await axios({
+      method: 'GET',
+      url: `http://localhost:5000/title/${title}`,
+    }))
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log(books);
+}
 
 module.exports.general = public_users;
